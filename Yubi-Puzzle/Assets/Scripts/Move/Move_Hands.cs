e1ac5408d1a321616f0c44ecd36e21752db497f5
Kim-game3 <zweilous387@icloud.com>
2025-09-08 16:44:04 +0900

    移動実装中
    2Dのアセットを入れたので一応コミットします

--- Yubi-Puzzle/Assets/Scripts/Move/Move_Hands.cs
+++ Yubi-Puzzle/Assets/Scripts/Move/Move_Hands.cs
@@ -3,59 +3,31 @@ using UnityEngine.InputSystem;
 
 public class Move_Hands : MonoBehaviour
 {
+    [Tooltip("‘¬“x")]
     [SerializeField] float Move_Speed;
+
+    [Tooltip("Žw‚ðŠi”[‚·‚é”z—ñ")]
     [SerializeField] GameObject[] Fingers;
+
+    Rigidbody2D rb;
     private InputAction little;
     private InputAction ring;
     private InputAction middle;
     private InputAction index;
     private InputAction tomb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         little = InputSystem.actions.FindAction("Little");
         ring = InputSystem.actions.FindAction("Ring");
         middle = InputSystem.actions.FindAction("Middle");
         index = InputSystem.actions.FindAction("Index");
         tomb = InputSystem.actions.FindAction("Tomb");
-        
     }
 
-    void FixedUpdate()
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
 
-        if (little.IsPressed())
-        {
-            Vector3 force = new Vector3(0, 0, -Move_Speed);
 
-            rb.AddForce(force);
-        }
 
-        if(ring.IsPressed()) 
-        {
-            Vector3 force = new Vector3(Move_Speed, 0, -Move_Speed);
-
-            rb.AddForce(force);
-        }
-
-        if(middle.IsPressed()) 
-        {
-            Vector3 force = new Vector3(Move_Speed, 0, 0);
-            rb.AddForce(force);
-        }
-
-        if(index.IsPressed())
-        {
-            Vector3 force = new Vector3(Move_Speed, 0, -Move_Speed);
-            rb.AddForce(force);
-        }
-
-        if(tomb.IsPressed())
-        {
-            Vector3 force = new Vector3(0, 0, -Move_Speed);
-            rb.AddForce(force);
-        }
-    }
 
 }
