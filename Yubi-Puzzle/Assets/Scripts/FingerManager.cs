4dd3705d857f8a6292d44abd5da549698f5b6a00
Notherthing95 <flyingconsole@icloud.com>
2025-09-09 12:17:57 +0900

    バグ修正
    カウントダウン中に画面をクリック出来る問題を修正

--- Yubi-Puzzle/Assets/Scripts/FingerManager.cs
+++ Yubi-Puzzle/Assets/Scripts/FingerManager.cs
@@ -5,6 +5,7 @@ public class FingerManager : MonoBehaviour
     Camera mainCam;
     public Assign_Finger[] Finger;   // HACK:名前変えたい
     public GameObject SelectFinger;
+    public GameObject CountdownWindow;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,63 +16,92 @@ public class FingerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (CountdownWindow.activeSelf == false)
         {
-            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray,out RaycastHit hit))
+            if (Input.GetMouseButtonDown(0))
             {
-                Assign_Finger finger = hit.collider.GetComponent<Assign_Finger>();
+                Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
 
-                if (finger != null && SelectFinger == null) 
+                if (Physics.Raycast(ray, out RaycastHit hit))
                 {
-                    SelectFinger = hit.collider.gameObject;
-                    finger.OnFingerClicked();
-                }
-                else if (hit.collider.CompareTag("Finger"))
-                {
-                    Debug.Log("Swap Fingers!");
-                    GameObject otherFinger = hit.collider.gameObject;
-                    FingerInfo infoA = SelectFinger.GetComponent<FingerInfo>();
-                    FingerInfo infoB = otherFinger.GetComponent<FingerInfo>();
+                    Assign_Finger finger = hit.collider.GetComponent<Assign_Finger>();
 
-                    //HitBoxの保存
-                    GameObject hitBoxA = infoA.currentHitBox;
-                    GameObject hitBoxB = infoB.currentHitBox;
-
-                    Vector3 tmp = finger.transform.position;
-                    finger.transform.position = hit.collider.gameObject.transform.position;
-                    hit.collider.gameObject.transform.position = tmp;
-
-                    infoA.currentHitBox = hitBoxB;
-                    infoB.currentHitBox = hitBoxA;
-
-                    for (int i = 0; i < Finger.Length; i++)
+                    if (finger != null && SelectFinger == null)
                     {
-                        Finger[i].isClick = false;
+                        SelectFinger = hit.collider.gameObject;
+                        finger.OnFingerClicked();
                     }
-                    SelectFinger = null;
+                    //else if (hit.collider.CompareTag("Finger"))
+                    //{
+                    //    Debug.Log("Swap Fingers!");
+                    //    GameObject otherFinger = hit.collider.gameObject;
+                    //    FingerInfo infoA = SelectFinger.GetComponent<FingerInfo>();
+                    //    FingerInfo infoB = otherFinger.GetComponent<FingerInfo>();
+
+                    //    //HitBoxの保存
+                    //    GameObject hitBoxA = infoA.currentHitBox;
+                    //    GameObject hitBoxB = infoB.currentHitBox;
+
+                    //    Vector3 tmp = finger.transform.position;
+                    //    finger.transform.position = hit.collider.gameObject.transform.position;
+                    //    hit.collider.gameObject.transform.position = tmp;
+
+                    //    infoA.currentHitBox = hitBoxB;
+                    //    infoB.currentHitBox = hitBoxA;
+
+                    //    for (int i = 0; i < Finger.Length; i++)
+                    //    {
+                    //        Finger[i].isClick = false;
+                    //    }
+                    //    SelectFinger = null;
+                    //}
+                    else if (hit.collider.CompareTag("HitBox"))
+                    {
+                        Debug.Log("HitBox is clicked");
+                        SetFingerPoint sfp = hit.collider.GetComponent<SetFingerPoint>();
+                        if (sfp != null && SelectFinger != null)
+                        {
+                            FingerInfo info = SelectFinger.GetComponent<FingerInfo>();
+                            if (info != null)
+                            {
+                                if (info.currentHitBox != null)
+                                {
+                                    info.currentHitBox.SetActive(true);
+                                    info.currentHitBox = null;
+                                }
+                                sfp.SetFinger(SelectFinger);
+                                hit.collider.gameObject.SetActive(false);
+                                info.currentHitBox = hit.collider.gameObject;
+                            }
+                        }
+
+                        for (int i = 0; i < Finger.Length; i++)
+                        {
+                            Finger[i].isClick = false;
+                        }
+                        SelectFinger = null;
+                    }
                 }
-                else if (hit.collider.CompareTag("HitBox"))
+                else
                 {
-                    Debug.Log("HitBox is clicked");
-                    SetFingerPoint sfp = hit.collider.GetComponent<SetFingerPoint>();
-                    if (sfp != null && SelectFinger != null)
+                    Debug.Log("Air Click");
+
+                    if (SelectFinger != null)
                     {
                         FingerInfo info = SelectFinger.GetComponent<FingerInfo>();
-                        if(info != null)
+                        if (info != null)
                         {
+                            // 指を元の位置に戻し、HitBoxを復活させる
+                            SelectFinger.transform.position = info.InitialPosition;
+
                             if (info.currentHitBox != null)
                             {
                                 info.currentHitBox.SetActive(true);
                                 info.currentHitBox = null;
                             }
-                            sfp.SetFinger(SelectFinger);
-                            hit.collider.gameObject.SetActive(false);
-                            info.currentHitBox = hit.collider.gameObject;
                         }
                     }
-
                     for (int i = 0; i < Finger.Length; i++)
                     {
                         Finger[i].isClick = false;
@@ -79,31 +109,6 @@ public class FingerManager : MonoBehaviour
                     SelectFinger = null;
                 }
             }
-            else
-            {
-                Debug.Log("Air Click");
-
-                if (SelectFinger != null)
-                {
-                    FingerInfo info = SelectFinger.GetComponent<FingerInfo>();
-                    if(info != null)
-                    {
-                        // 指を元の位置に戻し、HitBoxを復活させる
-                        SelectFinger.transform.position = info.InitialPosition;
-
-                        if(info.currentHitBox != null)
-                        {
-                            info.currentHitBox.SetActive(true);
-                            info.currentHitBox = null;
-                        }
-                    }
-                }
-                for (int i = 0; i < Finger.Length; i++)
-                {
-                    Finger[i].isClick = false;
-                }
-                SelectFinger = null;
-            }
-        }   
+        }
     }
 }
