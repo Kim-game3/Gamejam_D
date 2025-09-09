00aa924281d8ae1c112d9088d3d5b717d70a9ff1
Kim-game3 <zweilous387@icloud.com>
2025-09-09 10:25:19 +0900

    移動実装

--- Yubi-Puzzle/Assets/Scripts/Move/Move_Hands.cs
+++ Yubi-Puzzle/Assets/Scripts/Move/Move_Hands.cs
@@ -1,86 +1,59 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Move_Hands : MonoBehaviour
 {
-    [Tooltip("���x")]
+    [Tooltip("移動速度")]
     [SerializeField] float Move_Speed;
-
-    [Tooltip("�w���i�[����z��")]
-    [SerializeField] GameObject[] Fingers;
+    [Tooltip("どのキーを押したか表示するテキスト")]
+    [SerializeField] Text Debug_Text;
 
     Rigidbody2D rb;
-    private InputAction little;
-    private InputAction ring;
-    private InputAction middle;
-    private InputAction index;
-    private InputAction tomb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        little = InputSystem.actions.FindAction("Little");
-        ring = InputSystem.actions.FindAction("Ring");
-        middle = InputSystem.actions.FindAction("Middle");
-        index = InputSystem.actions.FindAction("Index");
-        tomb = InputSystem.actions.FindAction("Tomb");
     }
 
     private void FixedUpdate()
     {
-        if(little.IsPressed())
-        {
-            Debug.Log("Little button pressed");
-            while(!little.IsPressed()) 
-            {
-                rb.linearVelocity = new Vector2(0, Move_Speed);
-            }
+        Vector2 move = Vector2.zero;
 
-            rb.linearVelocity = Vector2.zero;
+        if(Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            move += new Vector2(0, -Move_Speed);
+            Debug_Text.text = $"{Key.A}";
         }
 
-        if(ring.IsPressed())
+        if(Keyboard.current.wKey.wasPressedThisFrame)
         {
-            Debug.Log("Ring button pressed");
-            while (!ring.IsPressed()) 
-            {
-                rb.linearVelocity = new Vector2(Move_Speed, Move_Speed);
-            }
-
-            rb.linearVelocity = Vector2.zero;
+            move += new Vector2(Move_Speed, -Move_Speed);
+            Debug_Text.text = $"{Key.W}";
         }
 
-        if (middle.IsPressed())
+        if(Keyboard.current.eKey.wasPressedThisFrame)
         {
-            Debug.Log("Middle button pressed");
-            while (!middle.IsPressed())
-            {
-                rb.linearVelocity = new Vector2(Move_Speed, 0);
-            }
-
-            rb.linearVelocity = Vector2.zero;
+            move += new Vector2(Move_Speed, 0);
+            Debug_Text.text = $"{Key.E}";
         }
 
-        if(!index.IsPressed())
+        if(Keyboard.current.fKey.wasPressedThisFrame) 
         {
-            Debug.Log("Index button pressed");
-            while (!index.IsPressed())
-            {
-                rb.linearVelocity = new Vector2(Move_Speed, -Move_Speed);
-            }
-
-            rb.linearVelocity = Vector2.zero;
+            move += new Vector2(Move_Speed, Move_Speed);
+            Debug_Text.text = $"{Key.F}";
         }
 
-        if(tomb.IsPressed())
+        if(Keyboard.current.spaceKey.wasPressedThisFrame) 
         {
-            Debug.Log("Tomb button pressed");
-            while (!tomb.IsPressed())
-            {
-                rb.linearVelocity = new Vector2(0, -Move_Speed);
-            }
+            move += new Vector2(0, Move_Speed);
+            Debug_Text.text = $"{Key.Space}";
+        }
 
-            rb.linearVelocity = Vector2.zero;
+        if(move != Vector2.zero)
+        {
+            rb.MovePosition(rb.position + move);
         }
     }
 
