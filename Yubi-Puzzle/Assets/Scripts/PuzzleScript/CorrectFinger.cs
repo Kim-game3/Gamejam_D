6714db95fbc5011bc592a5bff47df6df2302a050
SasakiTakehiro <s.takehiro1119@gmail.com>
2025-09-09 15:06:04 +0900

    リザルト画面の計算をしました

--- Yubi-Puzzle/Assets/Scripts/PuzzleScript/CorrectFinger.cs
+++ Yubi-Puzzle/Assets/Scripts/PuzzleScript/CorrectFinger.cs
@@ -6,6 +6,8 @@ public class CorrectFinger : MonoBehaviour
     public static class GameScore
     {
         public static int correctScore = 0;
+        public static int GoalTime = 0;//MoveScene‚Å‚©‚©‚Á‚½ŽžŠÔ
+        public static int RemainingTime = 0;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
